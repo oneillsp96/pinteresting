2eea3ef51d2089dab2dfebefc6fd60bc065b0802
Sean O'Neill <oneillsp96@gmail.com>
2015-01-19 11:29:18 -0500

    added contacts page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "pages#home"
   get "about" => "pages#about" #creates about_path
+  get "contact"=>"pages#contact"
+
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
